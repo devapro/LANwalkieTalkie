e1871094367650add35dc29eb12767b808a0d739
Arsenii Kharlanov <arsenyzp@gmail.com>
2020-08-21 15:49:56 +0300

    added foreground service

--- app/src/main/java/pro/devapp/walkietalkiek/WalkieTalkieApp.kt
+++ app/src/main/java/pro/devapp/walkietalkiek/WalkieTalkieApp.kt
@@ -1,6 +1,9 @@
 package pro.devapp.walkietalkiek
 
 import android.app.Application
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.os.Build
 import pro.devapp.walkietalkiek.data.DeviceInfoRepository
 import pro.devapp.walkietalkiek.service.ChanelController
 import timber.log.Timber
@@ -9,6 +12,10 @@ import timber.log.Timber.DebugTree
 class WalkieTalkieApp : Application() {
     lateinit var chanelController: ChanelController
     lateinit var deviceInfoRepository: DeviceInfoRepository
+
+    companion object {
+        const val CHANNEL_ID = "WalkieService"
+    }
 
     override fun onCreate() {
         super.onCreate()
@@ -22,5 +29,23 @@ class WalkieTalkieApp : Application() {
             applicationContext,
             deviceInfoRepository
         )
+
+        createNotificationChanel()
+    }
+
+    private fun createNotificationChanel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val notificationChannel = NotificationChannel(
+                CHANNEL_ID,
+                "WalkieTalkie",
+                NotificationManager.IMPORTANCE_HIGH
+            )
+            notificationChannel.setSound(null, null)
+            notificationChannel.setShowBadge(false)
+            notificationChannel.importance = NotificationManager.IMPORTANCE_LOW
+            getSystemService(NotificationManager::class.java)?.createNotificationChannel(
+                notificationChannel
+            )
+        }
     }
 }
--- app/src/main/java/pro/devapp/walkietalkiek/service/WalkieService.kt
+++ app/src/main/java/pro/devapp/walkietalkiek/service/WalkieService.kt
@@ -1,11 +1,22 @@
 package pro.devapp.walkietalkiek.service
 
+import android.app.Notification
+import android.app.PendingIntent
 import android.app.Service
 import android.content.Intent
+import android.os.Build
 import android.os.IBinder
+import androidx.core.app.NotificationCompat
+import pro.devapp.walkietalkiek.R
 import pro.devapp.walkietalkiek.WalkieTalkieApp
+import pro.devapp.walkietalkiek.WalkieTalkieApp.Companion.CHANNEL_ID
+import pro.devapp.walkietalkiek.ui.MainActivity
 
 class WalkieService : Service() {
+
+    companion object {
+        const val NOTIFICATION_ID = 233
+    }
 
     override fun onBind(intent: Intent?): IBinder? {
         return null
@@ -17,16 +28,45 @@ class WalkieService : Service() {
 
     override fun onCreate() {
         super.onCreate()
+        (application as WalkieTalkieApp).chanelController.startDiscovery()
     }
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
-        startForeground(0, null)
-        (application as WalkieTalkieApp).chanelController.startDiscovery()
+        startForeground(NOTIFICATION_ID, createNotification())
         return START_REDELIVER_INTENT
     }
 
     override fun onDestroy() {
         super.onDestroy()
         (application as WalkieTalkieApp).chanelController.stopDiscovery()
+    }
+
+    private fun createNotification(): Notification {
+        val notificationIntent = Intent(this, MainActivity::class.java)
+        val pendingIntent = PendingIntent.getActivity(
+            this,
+            0,
+            notificationIntent,
+            PendingIntent.FLAG_UPDATE_CURRENT
+        )
+
+        val builder = NotificationCompat.Builder(this)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            builder.setChannelId(CHANNEL_ID)
+        }
+        return builder.setContentTitle(applicationContext.resources.getString(R.string.app_name))
+//            .setContentText(timeStr)
+            .setSmallIcon(R.mipmap.ic_launcher)
+//            .setCustomContentView(customRemoteViews)
+            .setContentIntent(pendingIntent)
+            .setOngoing(true)
+            .setOnlyAlertOnce(true)
+            .setAutoCancel(false)
+            .setVisibility(NotificationCompat.VISIBILITY_PUBLIC)
+            //  .setDefaults(Notification.DEFAULT_ALL)
+            .setPriority(NotificationCompat.PRIORITY_HIGH)
+            .setCategory(NotificationCompat.CATEGORY_REMINDER)
+            .setSound(null)
+            .build()
     }
 }
