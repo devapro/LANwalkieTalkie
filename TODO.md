f9baa182b852ba7fce55ec9bc76162b60c9697fa
Arsenii Kharlanov <arsenyzp@gmail.com>
2020-08-21 16:25:35 +0300

    added runtime permission request

--- TODO.md
+++ TODO.md
@@ -1,10 +1,11 @@
 - ping
-- mic runtime permission request
+- mic runtime permission request +
 - send only if has active client
-- foreground notification
+- foreground notification +
 - synchronization
 - client list ui
 - multiple device test
+- added talk button to foreground notification
 
 https://github.com/aahlenst/android-audiorecord-sample
 //android.os.Process.setThreadPriority( Process.THREAD_PRIORITY_URGENT_AUDIO );
--- app/src/main/java/pro/devapp/walkietalkiek/ui/MainActivity.kt
+++ app/src/main/java/pro/devapp/walkietalkiek/ui/MainActivity.kt
@@ -9,6 +9,8 @@ import pro.devapp.walkietalkiek.R
 import pro.devapp.walkietalkiek.VoiceRecorder
 import pro.devapp.walkietalkiek.WalkieTalkieApp
 import pro.devapp.walkietalkiek.service.WalkieService
+import pro.devapp.walkietalkiek.utils.permission.Permission
+import pro.devapp.walkietalkiek.utils.permission.UtilPermission
 import java.nio.ByteBuffer
 import java.util.*
 
@@ -16,9 +18,12 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var voiceRecorder: VoiceRecorder
 
+    private val utilPermission = UtilPermission()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         val serviceIntent = Intent(this, WalkieService::class.java)
         startService(serviceIntent)
 
@@ -26,12 +31,37 @@ class MainActivity : AppCompatActivity() {
             (application as WalkieTalkieApp).chanelController.sendMessage(ByteBuffer.wrap("test ${Date().seconds}".toByteArray()))
         }
 
+        utilPermission.checkOrRequestPermissions(this, object : UtilPermission.PermissionCallback(
+            arrayOf(Permission.AUDIO_RECORD)
+        ) {
+            override fun onSuccessGrantedAll() {
+                startVoiceRecorder()
+            }
+        })
+
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        voiceRecorder.destroy()
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        utilPermission.onRequestPermissionsResult(requestCode, permissions, grantResults)
+    }
+
+    private fun startVoiceRecorder() {
         voiceRecorder = VoiceRecorder() {
             (application as WalkieTalkieApp).chanelController.sendMessage(ByteBuffer.wrap(it))
         }
         voiceRecorder.create()
 
-        talk.setOnTouchListener { v, event ->
+        talk.setOnTouchListener { _, event ->
             when (event.action) {
                 MotionEvent.ACTION_DOWN -> {
                     voiceRecorder.startRecord()
@@ -46,10 +76,5 @@ class MainActivity : AppCompatActivity() {
                 }
             }
         }
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        voiceRecorder.destroy()
     }
 }
