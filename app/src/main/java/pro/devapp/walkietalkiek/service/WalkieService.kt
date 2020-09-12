ad97ab1b5625abb6125ad28ef23acd7011ae7bd1
Arsenii Kharlanov <arsenyzp@gmail.com>
2020-09-12 17:18:41 +0300

    added PARTIAL_WAKE_LOCK

--- app/src/main/java/pro/devapp/walkietalkiek/service/WalkieService.kt
+++ app/src/main/java/pro/devapp/walkietalkiek/service/WalkieService.kt
@@ -1,11 +1,15 @@
 package pro.devapp.walkietalkiek.service
 
 import android.app.Service
+import android.content.Context
 import android.content.Intent
 import android.os.IBinder
+import android.os.PowerManager
 import pro.devapp.walkietalkiek.WalkieTalkieApp
 
 class WalkieService : Service() {
+
+    private var wakeLock: PowerManager.WakeLock? = null
 
     override fun onBind(intent: Intent?): IBinder? {
         return null
@@ -14,6 +18,7 @@ class WalkieService : Service() {
     override fun onCreate() {
         super.onCreate()
         (application as WalkieTalkieApp).chanelController.startDiscovery()
+        setWakeLock()
     }
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
@@ -27,13 +32,22 @@ class WalkieService : Service() {
     override fun onDestroy() {
         super.onDestroy()
         (application as WalkieTalkieApp).chanelController.stopDiscovery()
+        releaseWakeLock()
     }
 
     private fun setWakeLock() {
-
+        wakeLock =
+            (getSystemService(Context.POWER_SERVICE) as PowerManager).run {
+                newWakeLock(
+                    PowerManager.PARTIAL_WAKE_LOCK,
+                    "WalkieTalkyApp::ServiceWakelockTag"
+                ).apply {
+                    acquire()
+                }
+            }
     }
 
     private fun releaseWakeLock() {
-
+        wakeLock?.release()
     }
 }
